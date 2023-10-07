90920c5e8202ce3149a270677f9c1b4555da37c7
Alexandre Augusto Arduini Tavares <arduinitavares@gmail.com>
2023-10-07 22:02:34 +0000

    added lists and tuples

--- src/Sandbox.hs
+++ src/Sandbox.hs
@@ -2,5 +2,16 @@ module Sandbox where
 
 
 x = 3 + 7
-
 main = putStrLn "Hello, world"
+
+_MY_CONSTANT_ = "A"
+
+_PIKACHU_ :: (String, Int)
+
+_PIKACHU_ = ("PIKACHU", 25)
+
+_FIB_ :: [Int]
+_FIB_ = [0, 1, 1, 2, 3, 5, 8, 13]
+
+_TRIPLE_ :: (Int, String, Bool)
+_TRIPLE_ = (10, "test", False)
